d1449bf6bcb43e2efba4d79568d297ae90c5245a
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-03-29 21:28:39 +0300

    chore: remove unused test, debug, and utility scripts

--- admin/pages/auto_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['admin_id'] = 1;
-$_SESSION['role_id'] = 1;
-$_SESSION['full_name'] = 'Test Admin';
-$_SESSION['permissions'] = ['backups.php', 'dashboard.php'];
-header('Location: /usms/admin/pages/backups.php');
-exit;
--- read_logs.php
+++
@@ -1,8 +0,0 @@
-<?php
-$log = "C:/xampp/apache/logs/error.log";
-if (!file_exists($log)) {
-    die("Not found");
-}
-$lines = file($log);
-$last = array_slice($lines, -50);
-echo implode("", $last);
