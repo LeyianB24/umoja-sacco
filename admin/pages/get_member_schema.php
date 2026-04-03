846af4e3947a23ca86cd9726b6f7ba25a8fa4c11
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-04-03 14:10:26 +0300

    chore: remove unused file and associated references

--- admin/pages/get_member_schema.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'c:/xampp/htdocs/usms/config/app.php';
-$res = $conn->query("SHOW COLUMNS FROM members");
-while($row = $res->fetch_assoc()) {
-    echo $row['Field'] . ' ' . $row['Type'] . "\n";
-}
