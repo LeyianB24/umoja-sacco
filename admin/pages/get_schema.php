0da720844d13b6edf59148a6e946db8e8d71377a
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-04-03 14:40:36 +0300

    refactor: remove unused and deprecated codebase files

--- admin/pages/get_schema.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'c:/xampp/htdocs/usms/config/app.php';
-$res = $conn->query("SHOW COLUMNS FROM transactions");
-while($row = $res->fetch_assoc()) {
-    echo $row['Field'] . ' ' . $row['Type'] . "\n";
-}
--- admin/pages/get_share_settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'c:/xampp/htdocs/usms/config/app.php';
-$res = $conn->query("SELECT * FROM share_settings LIMIT 1");
-print_r($res->fetch_assoc());
-echo "\n";
-$res = $conn->query("SHOW COLUMNS FROM share_settings");
-while($row = $res->fetch_assoc()) echo $row['Field'] . ' ' . $row['Type'] . "\n";
