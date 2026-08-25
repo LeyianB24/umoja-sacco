a377214553243593b1a3de9db63ee8198bc071f9
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-08-25 13:39:42 +0300

    chore: remove temporary debug scripts, duplicate documentation, and obsolete cache files

--- backend/scripts/list_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = json_decode(file_get_contents(__DIR__ . '/schema_dump.json'), true);
-$keys = array_keys($data);
-sort($keys);
-foreach ($keys as $i => $k) {
-    echo ($i + 1) . ". $k\n";
-}
